8bed93c56c65815ecf9773ae185114dec3b8869d
pk-vishnu <vishnuvpk05@gmail.com>
2026-03-28 14:15:35 +0530

    add PublicProfileDto

---
+++ src/main/java/com/typingstatsvit/api/dto/PublicProfileDto.java
@@ -0,0 +1,15 @@
+package com.typingstatsvit.api.dto;
+
+public record PublicProfileDto(
+        String displayName,
+        String discordUsername,
+        String avatarUrl,
+        String mtUrl,
+        Boolean mtVerified,
+        Boolean collegeVerified,
+        String typeggUsername,
+        String linkedinUrl,
+        String githubUrl,
+        String instagramUrl
+) {
+}

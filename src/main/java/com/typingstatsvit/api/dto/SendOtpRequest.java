1a780ef0fd0ea050d1655d9842885883b0fcfcb0
pk-vishnu <vishnuvpk05@gmail.com>
2026-03-29 02:21:35 +0530

    fix: sendOtpRequest email regexp dto add vitap

--- src/main/java/com/typingstatsvit/api/dto/SendOtpRequest.java
+++ src/main/java/com/typingstatsvit/api/dto/SendOtpRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record SendOtpRequest(
         @Pattern(
-                regexp = "^[A-Za-z0-9._%+-]+@vitstudent\\.ac\\.in$",
+                regexp = "^[A-Za-z0-9._%+-]+@vit(ap)?student\\.ac\\.in$",
                 message = "Must be a valid VIT email address"
         )
         String email
